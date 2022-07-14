5ca71d8edaab77d54b047d169c1870cf7e652b66
samir93bj <samir93bj@gmail.com>
2022-07-14 18:17:53 -0300

    Implementacion de Schema - DTO (Joi)

---
+++ middlewares/validator.handler.js
@@ -0,0 +1,17 @@
+const boom = require('@hapi/boom');
+
+function validatorHandler(schema, property){
+
+  return(req, res, next) => {
+    const data  = req[property];
+    const { error } = schema.validate(schema);
+
+    if(error){
+      next(boom.badRequest(error));
+    }
+
+    next();
+  }
+}
+
+module.exports = validatorHandler;
