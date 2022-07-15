ef400dd4d1bf6b6f0591b5ab594213017fe6c720
samir93bj <samir93bj@gmail.com>
2022-07-15 19:01:53 -0300

    Implementacion de 'Category'

---
+++ schemas/category.schema.js
@@ -0,0 +1,34 @@
+//Schema o DTO (Data Transfer objects)
+const Joi = require('joi');
+
+//ID
+const id = Joi.string().uuid();
+
+//NAME
+const name = Joi.string().min(3).max(15).messages();
+
+//IMAGE
+const image = Joi.string().uri();
+
+//CREATE CATEGORY
+const createCategorySchema = Joi.object({
+    name : name.required(),
+    image : image.required()
+});
+
+//UPDATE CATEGORY
+const updateCategorySchema = Joi.object({
+  name : name,
+  image: image
+});
+
+//OBTENER CATEGORY
+const getCategorySchema = Joi.object({
+  id : id.required()
+});
+
+module.exports = {
+  createCategorySchema,
+  updateCategorySchema,
+  getCategorySchema
+}
