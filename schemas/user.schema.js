be2775a2966a38070e17e168a00dec26867646f5
samir93bj <samir93bj@gmail.com>
2022-07-15 22:58:36 -0300

    Implement Service User

---
+++ schemas/user.schema.js
@@ -0,0 +1,24 @@
+const Joi = require('joi');
+
+const id = Joi.number().integer();
+const email = Joi.string().email();
+const password = Joi.string().min(8);
+const role = Joi.string().min(5)
+
+const createUserSchema = Joi.object({
+  email: email.required(),
+  password: password.required(),
+  role: role.required()
+});
+
+const updateUserSchema = Joi.object({
+  email: email,
+  password:password,
+  role: role,
+});
+
+const getUserSchema = Joi.object({
+  id: id.required(),
+});
+
+module.exports = { createUserSchema, updateUserSchema, getUserSchema }
