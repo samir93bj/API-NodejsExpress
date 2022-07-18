a542c267452d1f1c985f47332e590528dedc7be9
samir93bj <samir93bj@gmail.com>
2022-07-18 19:07:46 -0300

    Implementacion dokcer - postgres - pgadmin

---
+++ libs/postgres.js
@@ -0,0 +1,23 @@
+const { Client } = require('pg');
+
+async function getConnection(){
+
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'platzi',
+    password: 'admin123'
+  });
+
+  await client.connect(err => {
+      if (err) {
+        console.error('connection error', err.stack)
+      } else {
+        console.log('connected')
+      }
+  });
+
+  return client;
+}
+
+module.exports = getConnection;
