270b1993144c47d80dbd4b3264f3d2ee4b2039c8
samir93bj <samir93bj@gmail.com>
2022-08-19 21:53:12 -0300

    Agregado de user admin en migraciones

--- db/migrations/20220720224219-create-user.js
+++ db/migrations/20220720224219-create-user.js
@@ -6,6 +6,17 @@ module.exports = {
 
   up:  async (queryInterface) => {
       await queryInterface.createTable(USER_TABLE, UserSchemna);
+
+      const hash = await bcrypt.hash('123456', 10);
+      await queryInterface.bulkInsert(USER_TABLE, [
+        {
+          username: 'admin',
+          email: 'admin@domain.com',
+          password: hash,
+          role: 'admin',
+          created_at: new Date()
+        }
+      ]);
   },
 
   down: async  (queryInterface)=> {
