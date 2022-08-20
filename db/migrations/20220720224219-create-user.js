5e0e4d387df54f714cbc0a36408495c010ba1b5b
samir93bj <samir93bj@gmail.com>
2022-08-19 23:54:54 -0300

    implement bycript

--- db/migrations/20220720224219-create-user.js
+++ db/migrations/20220720224219-create-user.js
@@ -1,20 +1,20 @@
 'use strict';
 
 const {UserSchemna , USER_TABLE} = require('./../models/user.model');
+const bcrypt = require('bcrypt');
 
 module.exports = {
 
   up:  async (queryInterface) => {
       await queryInterface.createTable(USER_TABLE, UserSchemna);
 
-      const hash = await bcrypt.hash('123456', 10);
+      const hash = await bcrypt.hash('adminadmin', 10);
       await queryInterface.bulkInsert(USER_TABLE, [
         {
-          username: 'admin',
           email: 'admin@domain.com',
           password: hash,
+          created_at: new Date(),
           role: 'admin',
-          created_at: new Date()
         }
       ]);
   },
