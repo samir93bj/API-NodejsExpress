184f8693b5e5ad43c7e84d9f38ef7693936aa7cb
samir93bj <samir93bj@gmail.com>
2022-08-19 22:26:49 -0300

    Modif model Product

--- db/models/product.model.js
+++ db/models/product.model.js
@@ -31,7 +31,7 @@ const ProductSchema = {
   },
   categoryId:{
     field:'category_id',
-    allowNull:false,
+    allowNull:true,
     type:DataTypes.INTEGER,
     references:{
       model: CATEGORY_TABLE,
