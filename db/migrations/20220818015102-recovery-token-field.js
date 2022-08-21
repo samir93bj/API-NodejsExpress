85842457cdc56a16e286c0c7431551e10b26eddb
samir93bj <samir93bj@gmail.com>
2022-08-20 22:20:53 -0300

    Correction migration recovery-token

--- db/migrations/20220818015102-recovery-token-field.js
+++ db/migrations/20220818015102-recovery-token-field.js
@@ -3,11 +3,11 @@ const {USER_TABLE} = require('../models/user.model');
 
 module.exports = {
   async up (queryInterface,Sequelize ) {
-    await queryInterface.addColumn(USER_TABLE, 'recovery_token', {
+  /*  await queryInterface.addColumn(USER_TABLE, 'recovery_token', {
       field: 'recovery_token',
       allowNull: true,
       type: Sequelize.DataTypes.STRING,
-    });
+    });*/
   },
 
   async down (queryInterface) {
