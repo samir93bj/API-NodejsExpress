c71a1ea7100e3bd85e5ef3b724b21c0a9bfc1c89
samir mahmud <samir93bj@gmail.com>
2023-02-11 16:37:10 -0300

    chore: add config coverage to e2e

--- e2e/app.e2e.test.js
+++ e2e/app.e2e.test.js
@@ -1,29 +1,26 @@
 const request = require('supertest')
-const express = require('express')
+const createApp = require('../src/app')
 
-const app = express()
-const api = request(app)
+let app
+let api
 let server
 
-app.get('/hello', (req, res) => {
-  res.status(200).json({ name: 'Samir Mahmud' })
-})
-
-beforeAll(() => {
-  server = app.listen(3000)
-})
-
-afterAll(done => {
-  server.close()
-  done()
-})
-
 describe('test for app', () => {
-  test('should GET /hello', async () => {
-    const response = await api.get('/hello')
+  beforeAll(() => {
+    app = createApp()
+    api = request(app)
+    server = app.listen(3000)
+  })
+
+  afterAll(done => {
+    server.close()
+    done()
+  })
+
+  test('should GET /products', async () => {
+    const response = await api.get('/api/v1/products?priceMin=0&priceMax=15000')
     expect(response).toBeTruthy()
     expect(response.headers['content-type']).toMatch('/json')
     expect(response.status).toBe(200)
-    expect(response.body.name).toBe('Samir Mahmud')
   })
 })
