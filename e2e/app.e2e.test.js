388d28d0c0629194e067e44fc70eee332def38e8
samir mahmud <samir93bj@gmail.com>
2023-02-11 15:35:28 -0300

    chore: config and test supertest

--- e2e/app.e2e.test.js
+++ e2e/app.e2e.test.js
@@ -1,5 +1,29 @@
-describe('suite tests', () => {
-  test('suite', () => {
-    expect(1 + 1).toBe(2)
+const request = require('supertest')
+const express = require('express')
+
+const app = express()
+const api = request(app)
+let server
+
+app.get('/hello', (req, res) => {
+  res.status(200).json({ name: 'Samir Mahmud' })
+})
+
+beforeAll(() => {
+  server = app.listen(3000)
+})
+
+afterAll(done => {
+  server.close()
+  done()
+})
+
+describe('test for app', () => {
+  test('should GET /hello', async () => {
+    const response = await api.get('/hello')
+    expect(response).toBeTruthy()
+    expect(response.headers['content-type']).toMatch('/json')
+    expect(response.status).toBe(200)
+    expect(response.body.name).toBe('Samir Mahmud')
   })
 })
